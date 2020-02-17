7c66ad75ebe4b611cd2dc7f01408e3a1d1290ebf
Niccolò Piazzesi <npiazzesi@gmail.com>
2020-02-17 15:20:30 +0100

    everything done,TO DO report

--- marriages.py
+++ marriages.py
@@ -1,14 +1,15 @@
 import copy
 
-def stable_marriage(nodes, devices):
-    
+def stable_marriage(nodes = [], devices = []):
+    """ forma le associazioni sotto forma di tuple(nodo, dispositivo)"""
     if len(nodes) != len(devices):
-        raise ValueError('Deve esserci lo stesso numero di nodi e dispositivi')
-    #copia le liste dei nodi di ciascun dispositivo
+        raise ValueError('Deve esserci lo stesso numero di nodi e dispositivi!')
+    #copia le liste dei nodi di ciascun dispositivo,in modo da evitare side effects
     copy_nodes = []
     for d in devices:
         copy_nodes.append(copy.copy(d.ranked_nodes))
     
+    #effettua l'associazione
     while any(d.matched==False for d in devices):
         for d in devices:
             if not d.matched:
@@ -18,7 +19,7 @@ def stable_marriage(nodes, devices):
             if len(node.proposals) != 0:
                 node.choose_device()
     
-    
+    #ripristina le liste di nodi
     for i in range(len(devices)):
         devices[i].ranked_nodes = copy_nodes[i]
     return list((node, node.device) for node in nodes)
